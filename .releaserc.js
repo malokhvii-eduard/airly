aab893b88695727c1f8ab726d0acb6873c0b63b1
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-11-29 10:06:09 +0200

    chore: add .releaserc.js

---
+++ .releaserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git'
+  ],
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'next',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true }
+  ]
+}
