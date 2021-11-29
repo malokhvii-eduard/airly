50d4d5d3b58f4b7ce997f76aa6c3a32d17cedf9e
Eduard Malokhvii <malokhvii.ee@gmail.com>
2021-11-29 11:39:14 +0200

    chore(release): add changelog generation

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,14 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/git'
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
   ],
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
